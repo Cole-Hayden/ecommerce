98bc8fbf272a2161392c69ffe9a307bb630cde15
Cole-Hayden <cwh8b4@gmail.com>
2020-06-05 16:40:40 -0500

    adding route skeleton

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     (
-      <div className="container">
-        <div className="row">
-          <div className=""col-6>Column one</div>
-        </div>
-      </div>
+      <React.Fragment>
+        <h3>hello from app</h3>
+      </React.Fragment>
     )
   );
 }
