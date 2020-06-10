d114336aadd6ff6a24c95ede0c98e846b15483b9
Cole-Hayden <cwh8b4@gmail.com>
2020-06-10 15:04:08 -0500

    Provider functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import styled from 'styled-components';
-
+import { ButtonContainer } from './Button';
 
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
 
             <Link to="/">
             <img src={logo} alt="store" className="navbar-brand"/>
@@ -31,14 +31,18 @@ export default class Navbar extends Component {
                     my cart
                 </ButtonContainer>
             </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
 
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.4rem;
-background:transparent;
-border:0.5rem solid var(--lightBlue);
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform:capitalize;
+    //1rem 20px
+}
 `;
+
